6db0c80b1a2d7950bf7f94efe8575c94511f19cf
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-04-12 13:59:12 +0700

    click me

---
+++ A.java
@@ -0,0 +1,8 @@
+package com.codewithme;
+
+public class A {
+    public int date=4;
+    public void msg(){
+        System.out.println("hi");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.codewithme;
+
+public class Main {
+
+    public static void main(String[] args) {
+        A obj=new A();
+        System.out.println(obj.date);;//error vi access modifier la private
+    }
+}
+
